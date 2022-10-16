1ef1420f99eee55f4e706cfe8d5568da4c59a7b8
Alessandro Candido <candido.ale@gmail.com>
2022-10-16 09:32:34 +0200

    Add TOML syntax highlighting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import 'nextra-theme-docs/style.css'
+import '../style/highlight.css'
+import Prism from 'prism-react-renderer/prism'
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+require('prismjs/components/prism-toml')
