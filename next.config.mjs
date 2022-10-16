e6d1e1bf81b2d90659322e63f4dedb70a2bcabb5
Alessandro Candido <candido.ale@gmail.com>
2022-10-16 13:55:12 +0200

    Fix image optimization issue
    See https://nextjs.org/docs/messages/export-image-api

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.js',
+  unstable_staticImage: true,
+})
+
+export default {
+  ...withNextra(),
+  images: { unoptimized: true },
+}
