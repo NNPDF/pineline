3e5b5283d311e31e81fcfeae202e377ad25e7af1
Alessandro Candido <candido.ale@gmail.com>
2023-02-25 11:57:24 +0100

    Revert base path addition

--- src/components/internal-link.tsx
+++ src/components/internal-link.tsx
@@ -1,13 +1,10 @@
-import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 import styles from './internal-link.module.css'
 
 export default function ({ href, children }) {
-  const router = useRouter();
-
   return (
-    <Link href={`${router.basePath}/${href}`}>
+    <Link href={href}>
       <a className={styles.link}>{children}</a>
     </Link>
   )
