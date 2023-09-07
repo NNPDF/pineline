0d96f20c62dc9662613fcf5ace50c9a3a89ae464
Alessandro Candido <candido.ale@gmail.com>
2023-09-07 19:15:05 +0200

    Add deps to fonll

--- fonll/assets/fonll/compare.py
+++ fonll/assets/fonll/compare.py
@@ -1,9 +1,10 @@
 import json
 import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-import seaborn as sns
 import pandas as pd
+import seaborn as sns
 from tabulate import tabulate
 
 
@@ -36,20 +37,22 @@ fonll_result = (
 
 to_table = [["Q2", "x", "new", "old", "percent_diff"]]
 for q2, xx, new, old in zip(q2, x, fonll_result, master_result):
-    if q2<3.49:
+    if q2 < 3.49:
         continue
-    to_table.append([q2, xx, new, old, ((new/old)-1.0)*100])
+    to_table.append([q2, xx, new, old, ((new / old) - 1.0) * 100])
 print(tabulate(to_table))
 
 
 ratio = (np.float64(fonll_result) / master_result).reshape(len(uniqx), len(uniqq2)).T
 ratio = np.flip(ratio, axis=0)
-pd_ratio = pd.DataFrame(ratio, columns=np.around(uniqx, 6), index=np.around(np.flip(uniqq2), 1))
+pd_ratio = pd.DataFrame(
+    ratio, columns=np.around(uniqx, 6), index=np.around(np.flip(uniqq2), 1)
+)
 
 
-import ipdb; ipdb.set_trace()
+__import__("pdb").set_trace()
 # ++++++++++++++++ 1d sf plot +++++++++++++++++++++++++++++++++++++++++
-single_x = np.where(x==uniqx[-1])[0]
+single_x = np.where(x == uniqx[-1])[0]
 # single_x=np.array([i for i in range(len(x))])
 # plt.plot(q2[single_x],new_result[:,0][single_x],"o",color="C2", label="FFNS3",alpha=0.5)
 # plt.plot(q2[single_x],new_result[:,1][single_x],"v",color="C3", label="FFN03",alpha=0.5)
@@ -61,13 +64,13 @@ single_x = np.where(x==uniqx[-1])[0]
 # plt.axvline(x = 5, label = 'POS scale', color='blue')
 # plt.axvline(x = 387.3, label = r'$(4m_b)^2$', color='black')
 
-plt.plot(x,new_result[:,0],"o",color="C2", label="FFNS3",alpha=0.5)
-plt.plot(x,new_result[:,1],"v",color="C3", label="FFN03",alpha=0.5)
-plt.plot(x,new_result[:,2],"^",color="C4", label="FFNS4",alpha=0.5)
+plt.plot(x, new_result[:, 0], "o", color="C2", label="FFNS3", alpha=0.5)
+plt.plot(x, new_result[:, 1], "v", color="C3", label="FFN03", alpha=0.5)
+plt.plot(x, new_result[:, 2], "^", color="C4", label="FFNS4", alpha=0.5)
 # plt.plot(x,new_result[:,3],">",color="C5", label="FFN04",alpha=0.5)
 # plt.plot(x,new_result[:,4],"<",color="C6", label="FFNS5",alpha=0.5)
-plt.plot(x,master_result,"s", color="C0", label="aFONLL",alpha=0.5)
-plt.plot(x,fonll_result,"P",color="C1", label="nFONLL",alpha=0.5)
+plt.plot(x, master_result, "s", color="C0", label="aFONLL", alpha=0.5)
+plt.plot(x, fonll_result, "P", color="C1", label="nFONLL", alpha=0.5)
 # plt.axvline(x = 5/(5+4*4.92**2), label = r'$\frac{Q^2}{Q^2+4m_b}$', color='black')
 
 plt.xscale("log")
@@ -101,22 +104,22 @@ plt.savefig("test.png")
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-fig = plt.figure(figsize=(13,10))
+fig = plt.figure(figsize=(13, 10))
 
 ax0 = fig.add_subplot(2, 2, 3)
 ax1 = fig.add_subplot(2, 2, 2)
 ax2 = fig.add_subplot(2, 2, 1)
 ax3 = fig.add_subplot(2, 2, 4)
-X, Y = np.meshgrid(uniqx,uniqq2)
+X, Y = np.meshgrid(uniqx, uniqq2)
 
 im0 = ax0.pcolormesh(
-        X,
-        Y,
-        (fonll_result / master_result).reshape(len(uniqx),len(uniqq2)).T,
-        shading="auto",
-        cmap=plt.colormaps["bwr"],
-        norm=colors.CenteredNorm(vcenter=1.0, halfrange=0.25),
-    )
+    X,
+    Y,
+    (fonll_result / master_result).reshape(len(uniqx), len(uniqq2)).T,
+    shading="auto",
+    cmap=plt.colormaps["bwr"],
+    norm=colors.CenteredNorm(vcenter=1.0, halfrange=0.25),
+)
 ax0.set_ylabel(r"$Q^2$")
 ax0.set_xlabel(r"$x$")
 ax0.set_xscale("log")
@@ -124,11 +127,11 @@ ax0.set_yscale("log")
 ax0.set_title("numerical fonll / analytical fonll")
 
 im1 = ax1.pcolormesh(
-        X,
-        Y,
-        np.log(master_result.reshape(len(uniqx),len(uniqq2))).T,
-        shading="auto",
-    )
+    X,
+    Y,
+    np.log(master_result.reshape(len(uniqx), len(uniqq2))).T,
+    shading="auto",
+)
 ax1.set_ylabel(r"$Q^2$")
 ax1.set_xlabel(r"$x$")
 ax1.set_xscale("log")
@@ -136,11 +139,11 @@ ax1.set_yscale("log")
 ax1.set_title("analytical fonll structure function")
 
 im2 = ax2.pcolormesh(
-        X,
-        Y,
-        np.log(fonll_result.reshape(len(uniqx),len(uniqq2))).T,
-        shading="auto",
-    )
+    X,
+    Y,
+    np.log(fonll_result.reshape(len(uniqx), len(uniqq2))).T,
+    shading="auto",
+)
 ax2.set_ylabel(r"$Q^2$")
 ax2.set_xlabel(r"$x$")
 ax2.set_xscale("log")
@@ -149,12 +152,12 @@ ax2.set_title("numerical fonll structure function")
 
 
 im3 = ax3.pcolormesh(
-        X,
-        Y,
-        ((master_result-fonll_result).reshape(len(uniqx),len(uniqq2))).T,
-        shading="auto",
-        cmap="plasma"
-    )
+    X,
+    Y,
+    ((master_result - fonll_result).reshape(len(uniqx), len(uniqq2))).T,
+    shading="auto",
+    cmap="plasma",
+)
 ax3.set_ylabel(r"$Q^2$")
 ax3.set_xlabel(r"$x$")
 ax3.set_xscale("log")
